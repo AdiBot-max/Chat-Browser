23a028202296727e43e273db0c7b00eccbc1fe24
AdiBot-max <adichauhan12354@gmail.com>
2025-11-09 18:32:35 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Login({ setUser }) {
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
+const BASE_URL = window.location.origin;
 
-  const handleLogin = async ()=>{
-    try{
-      const res = await axios.post("http://localhost:5000/auth/login",{username,password});
-      setUser({username: res.data.username});
-    }catch(err){alert(err.response?.data.error||"Error");}
+export default function Login({ setUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(`${BASE_URL}/auth/login`, { username, password });
+      setUser({ username: res.data.username });
+    } catch (err) {
+      alert(err.response?.data.error || "Error");
+    }
   };
 
   return (
     <div>
       <h3>Login</h3>
-      <input placeholder="Username" value={username} onChange={e=>setUsername(e.target.value)}/>
-      <input placeholder="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+      <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+      <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
